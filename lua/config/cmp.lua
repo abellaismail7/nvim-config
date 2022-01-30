dccf32428225d348448710555f5954c986bafe31
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-01-30 21:05:01 +0000

    clean: cmp configuration

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -4,19 +4,14 @@ if not present then
    return
 end
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.completeopt = {"menu", "menuone", "noselect"}
 local luasnip = require("luasnip")
 
 cmp.setup({
 
   snippet = {
      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
+        luasnip.lsp_expand(args.body)
      end,
   },
 
@@ -40,41 +35,24 @@ cmp.setup({
   },
 
   mapping = {
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+     ["<C-e>"] = cmp.mapping(function ()
+        luasnip.expand()
+     end,{"i", "s"}),
+     ["<C-y>"] = cmp.mapping(function ()
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm {
-       behavior = cmp.ConfirmBehavior.Replace,
-       select = true,
-    },
+     end,{"i", "s"}),
+     ["<C-Space>"] = cmp.mapping.complete(),
+     ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+     },
   },
 
   sources = {
      { name = 'luasnip' },
      { name = "nvim_lsp" },
      { name = "nvim_lua" },
-     { name = "buffer" },
+     { name = "buffer", keyword_length = 4 },
   },
 })
 
