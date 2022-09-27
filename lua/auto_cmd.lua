00af1ec3af5f5341e6b11e0671cc425d1bd2b008
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-09-27 01:52:56 +0000

    auto header protection for hpp too

--- lua/auto_cmd.lua
+++ lua/auto_cmd.lua
@@ -2,12 +2,15 @@
 local api = vim.api
 
 api.nvim_create_autocmd("BufEnter", {
-	pattern = "*.h",
+	pattern = {"*.h", "*.hpp"},
 	callback = function ()
 		if (api.nvim_buf_line_count(0) == 1
 			and api.nvim_buf_get_lines(0, 0, 1, 0)[1] == "")
 			then
-			print("yasaa");
+    		local yn = vim.fn.input('y/[N]: ', '')
+			if yn ~= 'y' then
+				return;
+			end
 			local filename = vim.fn.expand("%:t"):upper():gsub("%.", "_")
 			api.nvim_buf_set_lines(0, 0,1,0, {
 				"#ifndef " .. filename,
