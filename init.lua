949d7b12e8636121451466f476dd00a6a0bd36d0
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-10-15 16:28:29 +0000

    use pure lua to set options

--- init.lua
+++ init.lua
@@ -4,47 +4,63 @@ require "plug"
 
 vim.wo.numberwidth = 2
 vim.wo.relativenumber = true
+vim.o.number = true
+vim.wo.signcolumn = 'number'
 
-vim.g.ft_theme = "chadracula"
-vim.o.shortmess = vim.o.shortmess .. "c"
-vim.o.termguicolors = true
-vim.o.timeoutlen = 1000
-vim.o.wildmenu = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.breakindent = true
-vim.opt.undofile = true
-vim.wo.signcolumn = 'number'
-vim.o.cmdheight = 0
 
-vim.api.nvim_set_option("backspace", "indent,eol,start")
-vim.api.nvim_set_option("arabicshape", true)
-vim.api.nvim_set_option("encoding", "UTF-8")
-vim.api.nvim_set_option("splitright", true)
-vim.api.nvim_set_option("splitbelow", true)
-vim.api.nvim_set_option("hidden", true)
-vim.api.nvim_set_option("wildoptions", "pum")
-vim.api.nvim_set_option("pumblend", 5)
-vim.api.nvim_set_option("laststatus", 3)
-vim.api.nvim_set_option("wildmode", "longest:full,full")
-vim.api.nvim_set_option("mouse", "")
+vim.g.ft_theme = "onedark"
+vim.o.termguicolors = true
+vim.o.wildmenu = true
+vim.opt.undofile = true
+
+vim.opt.shortmess = "" -- vim.o.shortmess .. "c"
+vim.opt.laststatus = 3
+vim.opt.cmdheight = 0
+
+vim.opt.backspace = "indent,eol,start"
+vim.opt.arabicshape = true
+vim.opt.encoding = "UTF-8"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.hidden = true
+vim.opt.wildoptions = "pum"
+vim.opt.pumblend = 5
+vim.opt.wildmode = "longest:full,full"
+vim.opt.mouse = ""
+
+vim.opt.timeoutlen = 1000
+vim.opt.updatetime = 250
+
+vim.opt.fillchars = { eob = " " }
+vim.opt.encoding = "utf-8"
+
+
+vim.o.showbreak = "";
+
+ local signs = {
+   Error = " ",
+   Warn = " ",
+   Hint = " ",
+   Info = " ",
+}
+
+for type, icon in pairs(signs) do
+   local hl = "DiagnosticSign" .. type
+   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
 
 -- indent
 vim.api.nvim_set_option("autoindent",true)
 vim.api.nvim_set_option("smartindent",true)
 vim.api.nvim_set_option("expandtab", false)
 
-vim.cmd("set fillchars=eob:\\ ")
 vim.cmd('let mapleader = " "')
 
 local th = require("theme")
 th.change_theme(vim.g.ft_theme);
-
-
-vim.cmd("syntax on")
-vim.cmd("set listchars +=eol:↴")
-vim.g.termdebug_disasm_window = 10
-vim.g.termdebug_wide = 1
 
 require"keymaps"
 require"auto_cmd"
