f5ff72dfaf289a18acde3c03e76344eae0ac9e3a
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-10-15 16:29:08 +0000

    telescope borders is better

--- lua/themes/integrations/alpha.lua
+++ lua/themes/integrations/alpha.lua
@@ -298,57 +298,12 @@ return {
 	DevIconxz = { fg = colors.sun },
 	DevIconzip = { fg = colors.sun },
 	DevIconZig = { fg = colors.orange },
-	
-	TelescopeBorder = {
-	  fg = colors.darker_black,
-	  bg = colors.darker_black,
-	},
-	
-	TelescopePromptBorder = {
-	  fg = colors.black2,
-	  bg = colors.black2,
-	},
-	
-	TelescopePromptNormal = {
-	  fg = colors.white,
-	  bg = colors.black2,
-	},
-	
-	TelescopePromptPrefix = {
-	  fg = colors.red,
-	  bg = colors.black2,
-	},
-	
-	TelescopeNormal = { bg = colors.darker_black },
-	
-	TelescopePreviewTitle = {
-	  fg = colors.black,
-	  bg = colors.green,
-	},
-	
-	TelescopePromptTitle = {
-	  fg = colors.black,
-	  bg = colors.red,
-	},
-	
-	TelescopeResultsTitle = {
-	  fg = colors.darker_black,
-	  bg = colors.darker_black,
-	},
-	
-	TelescopeSelection = { bg = colors.black2, fg = colors.white },
-	
-	TelescopeResultsDiffAdd = {
-	  fg = colors.green,
-	},
-	
-	TelescopeResultsDiffChange = {
-	  fg = colors.yellow,
-	},
-	
-	TelescopeResultsDiffDelete = {
-	  fg = colors.red,
-	},
+
+	TelescopeBorder 		= { fg = colors.white, bg = theme.base00, },
+	TelescopePromptBorder 	= { fg = colors.white, bg = theme.base00, },
+	TelescopeResultsBorder	= { fg = colors.white, bg = theme.base00, },
+	TelescopePreviewBorder	= { fg = colors.white, bg = theme.base00, },
+	TelescopeNormal 		= { bg = theme.base00 },
 
 	WhichKey = { fg = colors.blue },
 	WhichKeySeparator = { fg = colors.light_grey },
