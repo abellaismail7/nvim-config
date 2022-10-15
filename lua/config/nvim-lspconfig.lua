8f37fb815258a27e16bd42d1dc57a02f05d5904b
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-10-15 16:29:39 +0000

    delete unsed func

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -1,7 +1,6 @@
 local nvim_lsp = require('lspconfig')
 
 local on_attach = function(_, bufnr)
-	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 	local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
 	--Enable completion triggered by <c-x><c-o>
