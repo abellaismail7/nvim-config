5b05d3a75c3b14a22dafc959a17dd0e978760915
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-10-16 08:29:49 +0000

    move keymaps to keymap.lua

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -1,14 +1,5 @@
 -- Mappings for nvimtree
 -- :au! BufEnter if &buftype == 'NvimTree' | setloca endif
-vim.api.nvim_set_keymap(
-    "n",
-    "<leader>n",
-    ":NvimTreeToggle<CR>",
-    {
-        noremap = true,
-        silent = true
-    }
-)
 
 require'nvim-tree'.setup {
     view = {
